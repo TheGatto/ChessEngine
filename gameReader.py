eef9c20b9c2c0e026d0c783a45c2545a9b29aa6b
Matiss Rault <matissrault@gmail.com>
2023-03-19 09:10:43 +0800

    Compacted code

--- gameReader.py
+++ gameReader.py
@@ -10,15 +10,9 @@ def readCode(code:str):
                 conv.append([8 - int(move[1]), alpha.index(move[0]), 8 - int(move[4]), alpha.index(move[3]), 'x'])
         else:
             if move == 'O-O':  # Kingside
-                if code.index(move) % 2 == 0:  # White's Turn
-                    conv.append([0, 0])
-                else:  # Black's Turn
-                    conv.append([1, 0])
+                conv.append([0, 0] if code.index(move) % 2 == 0 else [1, 0])
             elif move == 'O-O-O':  # Queenside
-                if code.index(move) % 2 == 0:  # White's Turn:
-                    conv.append([0, 1])
-                else:  # Black's turn
-                    conv.append([1, 1])
+                conv.append([0, 1] if code.index(move) % 2 == 0 else [1, 1])
     return conv
 if __name__ == '__main__':
     codeInput = input("Input code to be read: ")
